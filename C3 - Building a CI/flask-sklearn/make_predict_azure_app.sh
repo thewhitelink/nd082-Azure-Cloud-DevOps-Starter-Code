c3ebac0f1d7fdc1e2f875dd26f3d07700e64225f
thewhitelink <dwilson05lh@gmail.com>
2023-05-03 10:20:53 -0400

    undo test

--- C3 - Building a CI/flask-sklearn/make_predict_azure_app.sh
+++ C3 - Building a CI/flask-sklearn/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://project2app.azurewebsites.net:$PORT/
+     -X POST https://project2app.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
