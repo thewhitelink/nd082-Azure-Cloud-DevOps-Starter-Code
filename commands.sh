99e4e44ab90a640d08057d6a95c221eeb932c5c2
thewhitelink <dwilson05lh@gmail.com>
2023-05-04 14:56:21 -0400

    edited commands

--- commands.sh
+++ commands.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
+
 git clone https://github.com/thewhitelink/project2.git
+
 cd project2
 
 python3 -m venv ~/.project2
+
 source ~/.project2/bin/activate
+
 make install
-python -m flask run
+
+make all
 
 az webapp up --name project2app --resource-group Azuredevops --runtime "PYTHON:3.7"
 
 git pull
+
 ./make_predict_azure_app.sh
