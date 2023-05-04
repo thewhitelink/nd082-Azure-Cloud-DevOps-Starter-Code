77c6fd0a6d65699bd6a8f905290d320e2e616c33
thewhitelink <dwilson05lh@gmail.com>
2023-05-04 16:00:56 -0400

    changed app.py

--- app.py
+++ app.py
@@ -4,47 +4,44 @@ import logging
 
 import pandas as pd
 from sklearn.externals import joblib
-import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
+
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info(f"Scaling Payload: {payload}")
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
+
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home (version 2)</h3>"
     return html.format(format)
 
-# TO DO:  Log out the prediction value
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    # Performs an sklearn prediction
+    json_payload = request.json
 
     try:
-        #
-        # Load pretrained model as clf. Try any one model. 
         clf = joblib.load("LinearRegression.joblib")
-        #clf = joblib.load("StochasticGradientDescent.joblib")
     except:
-        LOG.info("JSON payload: %s json_payload")
+        LOG.info(f"JSON payload: {json_payload}")
         return "Model not loaded"
 
-    json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
+    LOG.info(f"JSON payload: {json_payload}")
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    LOG.info(f"Inference payload DataFrame: {inference_payload}")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+    return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
