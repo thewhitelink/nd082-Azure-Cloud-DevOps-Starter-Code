e687190959055a1dae3bc465a389ac841e54da35
thewhitelink <dwilson05lh@gmail.com>
2023-05-04 15:29:11 -0400

    created locust.py

---
+++ locust.py
@@ -0,0 +1,10 @@
+import time
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+        wait_time = between(1, 5)
+
+        @task
+        def index_page(self):
+            self.client.get(url="https://project2app.azurewebsites.net/")
